611eaa670e5bd068328ab76aae1391d632fa2efc
MacBook Pro <quoccuong.ukh97@gmail.com>
2023-05-05 20:15:58 +0700

    yes

--- immg.py
+++ immg.py
@@ -37,6 +37,18 @@ def download_audio_banmai():
             time.sleep(4)
             options = webdriver.ChromeOptions()
             options.add_argument("headless")
+            options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) 
+            options.add_argument("--no-sandbox") 
+            options.add_argument("--disable-setuid-sandbox") 
+
+            options.add_argument("--remote-debugging-port=9222")  # this
+
+            options.add_argument("--disable-dev-shm-using") 
+            options.add_argument("--disable-extensions") 
+            options.add_argument("--disable-gpu") 
+            options.add_argument("start-maximized") 
+            options.add_argument("disable-infobars")
+            options.add_argument(r"user-data-dir=.\cookies\\test") 
             web = webdriver.Chrome(options=options)
             # web = webdriver.Chrome()
             web.get('https://online.immi.gov.au/lusc/login')
